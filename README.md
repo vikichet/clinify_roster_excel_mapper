a91cf6b384e4b6a503f90974aa23765de9bc2bd0
vivekchetia <vivek@techvariable.com>
2024-11-21 15:05:13 +0530

    init maping util

---
+++ README.md
@@ -0,0 +1,5 @@
+# Clinify Util for Maping Payer Roster Fields
+
+This util can be used for maping the payer roster fields that generally is not correctly mapped or come combined with caregaps data.
+
+Simply run the html in live server and follow the steps
--- index.js
+++ index.js
@@ -110,21 +110,33 @@ function getHeaderRow(sheet) {
   return headers;
 }
 
-fileInput.addEventListener("change", (e) => {
+fileInput.addEventListener("change", (ev) => {
   const reader = new FileReader();
-  reader.readAsArrayBuffer(e.target.files[0]);
+  reader.readAsArrayBuffer(ev.target.files[0]);
 
-  reader.onload = function (e) {
+  reader.onload = async function (e) {
     const data = new Uint8Array(reader.result);
     const workbook = XLSX.read(data, { type: "array" });
     const ws = workbook.Sheets.Sheet1;
     const headers = getHeaderRow(ws);
-    const jsonData = XLSX.utils.sheet_to_json(ws);
+
+    const jsonData = await new Promise((resolve, reject) => {
+      Papa.parse(ev.target.files[0], {
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        error(error) {
+          reject({ errorMsg: error });
+        },
+        async complete(results) {
+          resolve(results);
+        },
+      });
+    });
 
     //-- CUSTOM MAPPING IF NEEDED --//
-    const [h, j] = CUSTOM_MAPPING(headers, jsonData);
+    const [h, j] = CUSTOM_MAPPING(headers, jsonData.data);
     //-- CUSTOM MAPPING IF NEEDED --//
-
     initialise(h, j);
   };
 });
@@ -144,25 +156,50 @@ function generateJSONDownloadLink(data) {
  * @param {Array<String>} headers
  */
 function initialise(headers = [], jsonData = []) {
-  console.log({ headers });
   console.log("INITIALIZING");
-
   const mainContainer = document.querySelector("main");
-
   if (mainContainer.children.length > 0) mainContainer.innerHTML = "";
 
   const form = document.createElement("form");
+  form.className = "grid-container";
 
   form.onsubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(form);
+
     setMappingStateFn(Object.fromEntries(formData));
-    reMapJsonData(jsonData);
+    const newReMappedData = reMapJsonData(jsonData);
+
+    const worksheet = XLSX.utils.json_to_sheet(newReMappedData);
+    const workbook = XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(workbook, worksheet);
+
+    const downloadBtn = document.createElement("button");
+
+    downloadBtn.innerText = "DOWNLOAD";
+    downloadBtn.id = "download-btn";
+
+    const csv = XLSX.utils.sheet_to_csv(worksheet, { strip: true });
+
+    // downloadFile(csv, "my_csv.csv", "text/csv;encoding:utf-8", form);
+
+    downloadBtn.className = "grid-item";
+    const existingDownloadBtn = document.getElementById("download-btn");
+
+    if (existingDownloadBtn) mainContainer.removeChild(existingDownloadBtn);
+    form.appendChild(downloadBtn);
+
+    downloadBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      XLSX.writeFile(workbook, "MAPPED.xlsx", { compression: true });
+    });
   };
 
   const submitButton = document.createElement("button");
   submitButton.type = "submit";
-  submitButton.textContent = "Parse";
+  submitButton.textContent = "Parse and Download";
+  submitButton.className = "gird-item";
 
   MAPPING_FILEDS.forEach((iKey) => {
     const label = document.createElement("label");
@@ -182,15 +219,11 @@ function initialise(headers = [], jsonData = []) {
 
     select.setAttribute("name", `${iKey}`);
 
-    label.style.marginLeft = "10px";
-    select.style.marginLeft = "10px";
-    select.style.marginBottom = "10px";
-    select.style.maxWidth = "100px";
-    const br = document.createElement("br");
+    label.className = "grid-item";
+    select.className = "grid-item";
 
     form.appendChild(label);
     form.appendChild(select);
-    form.appendChild(br);
   });
   form.appendChild(submitButton);
   mainContainer.appendChild(form);
@@ -202,13 +235,15 @@ function reMapJsonData(jsonData = []) {
     let mappedObj = {};
 
     MAPPING_FILEDS.forEach((key) => {
-      mappedObj[key] = data[getMappingStateFn()[key]];
+      if (key === "Patient Barriers")
+        mappedObj[key] = data[getMappingStateFn()[key]] ?? "N";
+      else mappedObj[key] = data[getMappingStateFn()[key]] ?? null;
     });
 
     mappedData.push(mappedObj);
   });
 
-  console.log({ mappedData });
+  return mappedData;
 }
 
 /**
@@ -285,57 +320,96 @@ function CUSTOM_MAPPING(headers, jsonData) {
     X: "Open",
   };
 
-  const toConsolidateKeysMap = {
-    "BPDB : Blood Pressure Control for Patients With Diabetes Non-Medicare  .":
-      "BP control (<140/90 mm Hg)",
-    "CDCRB : Comprehensive Diabetes Care Non-Medicare Retired - Non-Medicare HbA1c Test .":
-      "Haemoglobin A1c (HbA1c) testing",
-    "EEDB : Eye Exam for Patients with Diabetes Non-Medicare - Non-Medicare Eye Exam .":
-      "Eye exam (retinal) performed",
-    "HBDB : Hemoglobin A1c Control for Patients With Diabetes Non-Medicare - Non-Medicare HbA1c Control (<8) .":
-      "HbA1c control (<8.0%)",
-    "HBDB : Hemoglobin A1c Control for Patients With Diabetes Non-Medicare - Non-Medicare HbA1c Poor Control (>9) .":
-      "HbA1c poor control (>9.0%)",
-    "SPD : Statin Therapy for Patients With Diabetes - Non-Medicare Statin Adherence .":
-      "Statin Therapy for Patients With Diabetes Adherence",
-    "SPD : Statin Therapy for Patients With Diabetes - Non-Medicare Statin Therapy .":
-      "Statin Therapy for Patients With Diabetes Therapy",
-  };
+  // const toConsolidateKeysMap = {
+  //   "BPDB : Blood Pressure Control for Patients With Diabetes Non-Medicare  .":
+  //     "BP control (<140/90 mm Hg)",
+  //   "CDCRB : Comprehensive Diabetes Care Non-Medicare Retired - Non-Medicare HbA1c Test .":
+  //     "Haemoglobin A1c (HbA1c) testing",
+  //   "EEDB : Eye Exam for Patients with Diabetes Non-Medicare - Non-Medicare Eye Exam .":
+  //     "Eye exam (retinal) performed",
+  //   "HBDB : Hemoglobin A1c Control for Patients With Diabetes Non-Medicare - Non-Medicare HbA1c Control (<8) .":
+  //     "HbA1c control (<8.0%)",
+  //   "HBDB : Hemoglobin A1c Control for Patients With Diabetes Non-Medicare - Non-Medicare HbA1c Poor Control (>9) .":
+  //     "HbA1c poor control (>9.0%)",
+  //   "SPD : Statin Therapy for Patients With Diabetes - Non-Medicare Statin Adherence .":
+  //     "Statin Therapy for Patients With Diabetes Adherence",
+  //   "SPD : Statin Therapy for Patients With Diabetes - Non-Medicare Statin Therapy .":
+  //     "Statin Therapy for Patients With Diabetes Therapy",
+  // };
 
   toDeleteKeys.forEach((dKey) => {
     const dIdx = mHeaders.indexOf(dKey);
     mHeaders.splice(dIdx, 1);
   });
 
+  const toReFormate = ["Phone"];
+
   let newData = [];
-
-  mJsonData.forEach((jData) => {
-    let modObj = { ...jData };
-
-    toConsolidateKeys.forEach((cKey) => {
+  for (let i = 0; i < mJsonData.length; i++) {
+    let modObj = structuredClone(mJsonData[i]);
+    if (!modObj["Phone"]) {
+      console.log("NO PHONE");
+      continue;
+    }
+    // delete all the consolidating keys
+    for (let j = 0; j < toConsolidateKeys.length; j++) {
+      const cKey = toConsolidateKeys[j];
       delete modObj[cKey];
-      modObj["Care Opportunity"] = toConsolidateKeysMap[cKey] ?? null;
-      modObj["Care Opportunity Status"] =
-        toConsolidateMark[jData[cKey]] ?? null;
-    });
+    }
 
-    newData.push(modObj);
+    for (let k = 0; k < toConsolidateKeys.length; k++) {
+      const cKey = toConsolidateKeys[k];
+      modObj = structuredClone(modObj);
+      modObj["Care Opportunity"] = cKey;
+      modObj["Care Opportunity Status"] =
+        toConsolidateMark[mJsonData[i][cKey]] ?? null;
+      newData.push(modObj);
+    }
+  }
+
+  newData = newData.map((nObj) => {
+    toReFormate.forEach((fKey) => {
+      nObj[fKey] = formatPhoneNumber(nObj[fKey], false);
+    });
+    return nObj;
   });
 
-  console.log(newData);
+  mHeaders.push("Care Opportunity");
+  mHeaders.push("Care Opportunity Status");
 
-  return [mHeaders, mJsonData];
+  return [mHeaders, newData];
 }
 
-// let a = {
-//   "Foot Examination": null,
-//   "HbA1c control (<8.0%)": null,
-//   "BP control (<140/90 mm Hg)": null,
-//   "HbA1c poor control (>9.0%)": null,
-//   "Eye exam (retinal) performed": null,
-//   "Breast Cancer Screening (BCS)": null,
-//   "Haemoglobin A1c (HbA1c) testing": null,
-//   "Controlling High Blood Pressure (CBP)": null,
-//   "Statin Therapy for Patients With Diabetes Adherence": null,
-//   "Statin Therapy for Patients With Diabetes Therapy": null,
-// };
+function formatPhoneNumber(phoneNumberString, randomMask = false) {
+  if (randomMask)
+    return Math.floor(1000000000 + Math.random() * 9000000000) + "";
+  const cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+  return cleaned;
+}
+
+function downloadFile(content, fileName, mimeType, container) {
+  const a = document.createElement("a");
+  mimeType = mimeType || "application/octet-stream";
+  a.className = "grid-item";
+  if (navigator.msSaveBlob) {
+    navigator.msSaveBlob(
+      new Blob([content], {
+        type: mimeType,
+      }),
+      fileName
+    );
+  } else if (URL && "download" in a) {
+    a.href = URL.createObjectURL(
+      new Blob([content], {
+        type: mimeType,
+      })
+    );
+    a.setAttribute("download", fileName);
+    container.appendChild(a);
+    a.click();
+    container.removeChild(a);
+  } else {
+    location.href =
+      "data:application/octet-stream," + encodeURIComponent(content); // only this mime type is supported
+  }
+}
